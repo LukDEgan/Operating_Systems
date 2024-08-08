b769026244d412a1bbf29cf6759a1608fe16e4c3
Luke Egan <lukeegan2003@gmail.com>
2024-08-08 12:46:38 +0930

    bg working

--- minishell.c
+++ minishell.c
@@ -1,10 +1,10 @@
 /*********************************************************************
    Program  : miniShell                   Version    : 1.3
  --------------------------------------------------------------------
-   skeleton code for linix/unix/minix command line interpreter
+   skeleton code for linux/unix/minix command line interpreter
  --------------------------------------------------------------------
-   File			: minishell.c
-   Compiler/System	: gcc/linux
+   File         : minishell.c
+   Compiler/System    : gcc/linux
 
 ********************************************************************/
 
@@ -18,6 +18,7 @@
 
 #define NV 20  /* max number of command tokens */
 #define NL 100 /* input buffer size */
+#define MAX_JOBS 100
 char line[NL]; /* command input buffer */
 
 /*
@@ -25,8 +26,33 @@ char line[NL]; /* command input buffer */
  */
 
 void prompt(void) {
-  // fprintf(stdout, "\n msh> ");
+  // printf("\nmsh> ");
   fflush(stdout);
+}
+
+struct bg_job {
+  int pid;
+  int id;
+  char command[NL];
+};
+
+struct bg_job Jobs[MAX_JOBS];
+int job_count = 0;
+
+void check_background_jobs() {
+  int status;
+  pid_t wpid;
+  for (int i = 0; i < job_count; i++) {
+    if ((wpid = waitpid(Jobs[i].pid, &status, WNOHANG)) > 0) {
+      printf("[%d]+ Done %s\n", Jobs[i].id, Jobs[i].command);
+      // Remove the job from the list
+      for (int j = i; j < job_count - 1; j++) {
+        Jobs[j] = Jobs[j + 1];
+      }
+      job_count--;
+      i--;
+    }
+  }
 }
 
 int main(int argk, char *argv[], char *envp[])
@@ -36,24 +62,24 @@ int main(int argk, char *argv[], char *envp[])
 
 {
   int frkRtnVal;       /* value returned by fork sys call */
-  int wpid;            /* value returned by wait */
   char *v[NV];         /* array of pointers to command line tokens */
-  char *sep = " \t\n"; /* command line token separators    */
+  char *sep = " \t\n"; /* command line token separators */
   int i;               /* parse index */
   int bg;              /* background process */
-  int *status = 0;
+  int status;
 
-  /* prompt for and process one command line at a time  */
-
+  /* prompt for and process one command line at a time */
   while (1) { /* do Forever */
     prompt();
-    fgets(line, NL, stdin);
+    if (fgets(line, NL, stdin) == NULL) {
+      perror("fgets");
+      continue;
+    }
     fflush(stdin);
 
-    if (feof(stdin)) { /* non-zero on EOF  */
-
-      /* fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
-      feof(stdin), ferror(stdin)); exit(0); */
+    if (feof(stdin)) { /* non-zero on EOF */
+      fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(), feof(stdin),
+              ferror(stdin));
       exit(0);
     }
     if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
@@ -85,37 +111,37 @@ int main(int argk, char *argv[], char *envp[])
       }
       continue; /* to prompt */
     }
-
+    check_background_jobs();
     /* fork a child process to exec the command in v[0] */
     switch (frkRtnVal = fork()) {
       case -1: /* fork returns error to parent process */
-      {
         perror("fork");
         break;
-      }
       case 0: /* code executed only by child process */
-      {
         if (execvp(v[0], v) == -1) {
           perror("execvp");
           exit(EXIT_FAILURE);
         }
-      }
+        break;
       default: /* code executed only by parent process */
-      {
         if (!bg) {
-          if (waitpid(frkRtnVal, status, 0) == -1) {
+          if (waitpid(frkRtnVal, &status, 0) == -1) {
             perror("waitpid");
           }
         } else {
-          printf("[%d] %s started in background\n", frkRtnVal, v[0]);
+          printf("[%d] %d\n", job_count + 1, frkRtnVal);
+          Jobs[job_count].id = job_count + 1;
+          Jobs[job_count].pid = frkRtnVal;
+          strcpy(Jobs[job_count].command, v[0]);
+          for (int i = 1; i < NV; i++) {
+            if (v[i] == NULL) break;
+            strcat(Jobs[job_count].command, " ");
+            strcat(Jobs[job_count].command, v[i]);
+          }
+          job_count++;
         }
         break;
-      }
     } /* switch */
-    /* Check for any finished background processes */
-    while ((wpid = waitpid(-1, status, WNOHANG)) > 0) {
-      printf("[%d] Background process done\n", wpid);
-    }
   } /* while */
   return 0;
 } /* main */
